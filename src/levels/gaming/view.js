dcd2efc6cff5354a927cb908b30aec449b618681
jwLi <2938897783@qq.com>
2026-07-30 00:40:48 +0800

    修复合并后的关卡提示回归

--- src/levels/gaming/view.js
+++ src/levels/gaming/view.js
@@ -1,34 +1,9 @@
 const MISS_HINT = "注意主角的面朝方向，靠近一点再打！";
 
-function createGuideElement() {
-  const guide = document.createElement("div");
-  guide.className = "tutorial-joystick-guide";
-  guide.setAttribute("aria-hidden", "true");
-  guide.innerHTML = `
-    <div class="tutorial-joystick-pulse"></div>
-    <div class="tutorial-finger"></div>
-    <div class="tutorial-arrow arrow-up"></div>
-    <div class="tutorial-arrow arrow-down"></div>
-    <div class="tutorial-arrow arrow-left"></div>
-    <div class="tutorial-arrow arrow-right"></div>
-  `;
-  return guide;
-}
-
-function removeGuide() {
-  const guide = document.querySelector("#joystick .tutorial-joystick-guide");
-  if (guide) guide.remove();
-}
-
 export function showMoveTutorial(_ui) {
-  removeGuide();
-  const joystick = document.getElementById("joystick");
-  if (!joystick) return;
-  joystick.appendChild(createGuideElement());
 }
 
 export function showAttackTutorial(_ui) {
-  removeGuide();
 }
 
 export function showMissHint(ui) {
@@ -45,6 +20,5 @@ export function hideMissHint(ui) {
 
 export function hideTutorialOverlays(ui) {
   ui.hideOverlay("tutorialSubtitle");
-  removeGuide();
   ui.hideOverlay("tutorialMissHint");
 }
