77e7283eab6fec9a6817ccb7dcff7eceaebedf0c
Cuileiming <2063717708@qq.com>
2026-07-30 00:32:13 +0800

    修复教程关摇杆引导箭头：点击屏幕后显示，踩到光圈后消失

--- src/levels/gaming/view.js
+++ src/levels/gaming/view.js
@@ -1,11 +1,34 @@
 const MISS_HINT = "注意主角的面朝方向，靠近一点再打！";
 
-export function showMoveTutorial(ui) {
-  ui.hideOverlay("tutorialJoystickGuide");
+function createGuideElement() {
+  const guide = document.createElement("div");
+  guide.className = "tutorial-joystick-guide";
+  guide.setAttribute("aria-hidden", "true");
+  guide.innerHTML = `
+    <div class="tutorial-joystick-pulse"></div>
+    <div class="tutorial-finger"></div>
+    <div class="tutorial-arrow arrow-up"></div>
+    <div class="tutorial-arrow arrow-down"></div>
+    <div class="tutorial-arrow arrow-left"></div>
+    <div class="tutorial-arrow arrow-right"></div>
+  `;
+  return guide;
 }
 
-export function showAttackTutorial(ui) {
-  ui.hideOverlay("tutorialJoystickGuide");
+function removeGuide() {
+  const guide = document.querySelector("#joystick .tutorial-joystick-guide");
+  if (guide) guide.remove();
+}
+
+export function showMoveTutorial(_ui) {
+  removeGuide();
+  const joystick = document.getElementById("joystick");
+  if (!joystick) return;
+  joystick.appendChild(createGuideElement());
+}
+
+export function showAttackTutorial(_ui) {
+  removeGuide();
 }
 
 export function showMissHint(ui) {
@@ -22,6 +45,6 @@ export function hideMissHint(ui) {
 
 export function hideTutorialOverlays(ui) {
   ui.hideOverlay("tutorialSubtitle");
-  ui.hideOverlay("tutorialJoystickGuide");
+  removeGuide();
   ui.hideOverlay("tutorialMissHint");
 }
